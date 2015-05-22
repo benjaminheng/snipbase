a46126a9c9e63ecc451b7f3c7fa9b32b499c0d9f
Ben <benjheng@gmail.com>
2015-05-22 17:06:00 -0400

    Changed header layout.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:info] = "Successfully registered!"
+            log_in @user
             redirect_to root_path
         else
             render 'register'
