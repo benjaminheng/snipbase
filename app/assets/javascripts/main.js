cbadd7f7d2e093a1d52322f15cd6d02b1c4dc96f
Timothy <ttlj92@msn.com>
2015-05-25 10:25:17 +0800

    delete button only show when snippet_files > 1

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,20 @@ var ready = function() {
         var files = $("#new_snippet > .files");
         files.append($("#snippet_file_template").html());
         $(document).scrollTop(files.find(".file").last().offset().top - 100);
+    	toggleDeleteButton();
     });
+}
+
+var toggleDeleteButton = function(){
+	var totalFiles = $(".file").length;
+	var button = $(".file > .close")
+	
+	if (totalFiles <= 1) {
+		button.hide();
+	} else {
+		button.show();
+	}
+		
 }
 
 // fix for turbo-links preventing .ready() from working correctly. replaces .ready()
@@ -12,4 +25,5 @@ $(document).on('page:change', ready);
 $(document).on('click', '.close', function(){ 
    	var parent = $(this).parent();
    	parent.remove();
+   	toggleDeleteButton();
 });
