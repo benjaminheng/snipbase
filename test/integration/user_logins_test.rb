6bf74eb7ab95b12d6fae6f4db8235aded379278d
Ben <benjheng@gmail.com>
2015-05-25 22:16:01 +0800

    Added log_in_as(user) test helper for logins inside integration tests

--- test/integration/user_logins_test.rb
+++ test/integration/user_logins_test.rb
@@ -4,9 +4,10 @@ class UserLoginsTest < ActionDispatch::IntegrationTest
     include SessionsHelper
 
     test "login with invalid credentials" do
+        user = users(:user_1)
         get login_path
         assert_template 'sessions/login'
-        post login_path, session: {username: 'NonexistantUser', password: 'Password'}
+        log_in_as user, password: "InvalidPassword"
         assert_template 'sessions/login'
         get root_path
         assert_not logged_in?
@@ -15,7 +16,7 @@ class UserLoginsTest < ActionDispatch::IntegrationTest
     test "login with valid credentials" do
         user = users(:user_1)
         get login_path
-        post login_path, session: {username: user.username, password: 'password1'}
+        log_in_as user
         assert_redirected_to root_path
         assert logged_in?
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,23 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Simulate a login in as the specified user
+    def log_in_as(user, options = {})
+        password = options[:password] || "Password"
+        # Simulate a post request if inside an integration test,
+        # else set the session store
+        if integration_test?
+            post login_path, session: { username: user.username, password: password }
+        else
+            session[:user_id] = user.id
+        end
+    end
+
+    # Returns true if inside integration test
+    def integration_test?
+        defined?(post_via_redirect)
+    end
 end
