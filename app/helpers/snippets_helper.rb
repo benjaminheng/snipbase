b68cef6c4863a570d990bef9407c7a557c52a893
Timothy <ttlj92@msn.com>
2015-06-02 10:34:19 +0800

    Cleaned snippet helper

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,11 +1,2 @@
 module SnippetsHelper
-	def has_edit_permission
-		snippet = Snippet.find(params[:id])
-		current_user == snippet.user
-	end
-
-	def has_delete_permission
-		snippet = Snippet.find(params[:id])
-		current_user == snippet.user
-	end
 end
