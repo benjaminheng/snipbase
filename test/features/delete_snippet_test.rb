064e1c62d5eb1b696208328a57743f67704b7a95
Timothy <ttlj92@msn.com>
2015-06-08 16:52:31 +0800

    Added delete test snippet

---
+++ test/features/delete_snippet_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+feature "DeleteSnippet" do
+	before do
+        @snippet = create(:snippet)
+        @user = @snippet.user
+    end
+    
+    # In progress. Permissions not yet set
+    #scenario "Not logged in" do
+    #    visit show_snippet_path(@snippet)
+    #    current_path.must_equal login_path, "User not redirected to login page."
+    #end
+
+    scenario "Delete success", js: true do
+    	login_and_goto_show
+    	find(".snippet-delete-link").click
+
+        # Force capybara to wait for javascript redirect to finish
+        page.has_text?("Content can't be blank")
+
+    	current_path.must_equal add_path, "User not redirected to add"
+    	page.has_text?("Deleted snippet.").must_equal true
+    end
+
+    def login_and_goto_show
+        log_in_as @user
+        visit show_snippet_path(@snippet)
+    end
+end
