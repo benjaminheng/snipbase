4fd94391df779d2be47d2dcfe15ce176b38263e1
Ben <benjheng@gmail.com>
2015-06-13 20:14:06 +0800

    Added validations for snippet groups

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -2,6 +2,7 @@ class Snippet < ActiveRecord::Base
     has_many :snippet_files, :dependent => :destroy
     has_many :group_snippets, dependent: :destroy
     has_many :groups, through: :group_snippets, dependent: :destroy
+    validate :groups_valid
 
     belongs_to :user
     validates_presence_of :title
@@ -10,4 +11,12 @@ class Snippet < ActiveRecord::Base
     scope :priv, -> (priv) { where priv: priv }
     scope :permission, -> (current_user) { where user: current_user }
 
+    def groups_valid
+        groups.each do |group|
+            unless self.user.active_groups.include?(group)
+                errors.add :groups, "contain one you do not have permission for"
+                return
+            end
+        end
+    end
 end
