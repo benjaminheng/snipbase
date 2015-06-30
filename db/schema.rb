0e7397f47fb109ceae5a2ea659aaf328a24e13ea
Timothy <ttlj92@msn.com>
2015-06-30 13:33:02 +0800

    Added the migration files....

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630050709) do
+ActiveRecord::Schema.define(version: 20150630052204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,9 +82,9 @@ ActiveRecord::Schema.define(version: 20150630050709) do
     t.string   "password_digest"
     t.string   "email"
     t.string   "name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.boolean  "is_admin"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "is_admin",        default: false
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
