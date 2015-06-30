3d592b87a0f4727359e9f092340d1363d0e5c3a0
Timothy <ttlj92@msn.com>
2015-06-30 13:11:25 +0800

    Added is_admin to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611125532) do
+ActiveRecord::Schema.define(version: 20150630050709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20150611125532) do
     t.string   "name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
